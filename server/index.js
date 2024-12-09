ba55ee82eda74b528d5e7033b5a35e9890e11b7c
Raven Hendricks <ravenkbeats@gmail.com>
2024-12-09 17:21:30 -0500

    fix syntax in init function

--- server/index.js
+++ server/index.js
@@ -1,19 +1,24 @@
 const {
     client,
-    createTables,
+    createTables
 } = require('./db');
 const express = require('express');
 const app = express();
 app.use(express.json());
 
+/*app.use((err, req, res, next)=> {
+    console.log(err);
+    res.status(err.status || 500).send({ error: err.message ? err.message : err });
+  });
+*/
 const init = async()=> {
     const port = process.env.PORT || 3000;
     await client.connect();
-    console.log('You are now connected to the database.')
-}
-
-await createTables();
-console.log('You have created tables.');
-app.listen(port, ()=> console.log(`Listening on port ${PORT}.`));
+    console.log('You are now connected to the database.');
+    await createTables();
+    
+    console.log('You have created tables.');
+    app.listen(port, ()=> console.log(`Listening on port ${port}.`));
+};
 
 init();
