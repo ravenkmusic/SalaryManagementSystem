289d3d79d4f7e5056ca4e1740bb3d0d0429a8ea4
Raven Hendricks <ravenkbeats@gmail.com>
2024-12-09 17:20:58 -0500

    created sample table in database

--- server/db.js
+++ server/db.js
@@ -1,3 +1,21 @@
 const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/salary_management_system');
 const uuid = require('uuid');
+
+const createTables = async()=>{
+    const SQL = `
+        DROP TABLE IF EXISTS users;
+        CREATE TABLE users(
+            id UUID PRIMARY KEY,
+            adminhr VARCHAR(20) UNIQUE NOT NULL,
+            accountant VARCHAR(20) UNIQUE NOT NULL,
+            employee VARCHAR(20) UNIQUE NOT NULL
+        );
+    `;
+    await client.query(SQL);
+};
+
+module.exports = {
+    client,
+    createTables
+};
